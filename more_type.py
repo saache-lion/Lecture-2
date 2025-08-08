c6d5a5d2acb491107aff0b1aa7b542b5859337ea
Saache Lion <saachekapoor962011@gmail.com>
2025-08-08 21:06:57 +0530

    lists

---
+++ more_type.py
@@ -0,0 +1,26 @@
+
+'''
+arr = []
+arr.append(1)
+arr.append(2)
+arr.append(356)
+arr.append(489)       
+arr.append(5230)
+
+#sort the list in descending order
+
+arr.sort(reverse=True)
+print("Sorted array: ", arr)
+
+marks = {}
+print(type(marks))
+marks['Saache'] = 90
+marks['John'] = 85
+marks['Doe'] = 78
+marks['Alice'] = 92
+print("Marks Dictionary: ", marks)
+'''
+sum = {}
+arr = [1, 2, 3, 4, 5]
+sum[arr] = 6
+print("Sum Dictionary: ", sum)
