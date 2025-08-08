7b201e6810e9c7025b6609b14d0a1eef09dc5fa3
Saache Lion <saachekapoor962011@gmail.com>
2025-08-08 21:21:23 +0530

    ex 1

---
+++ ex.py
@@ -0,0 +1,8 @@
+#1
+
+fav_no = int(input("Enter your favorite number: "))
+print("Your favorite number is: ", fav_no)
+new_fav_no = fav_no * 2
+print("Your new favorite number is: ", new_fav_no)
+
+
