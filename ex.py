f5d254dc4f4ab9073e3279f91db86c8fc6eb53f0
Saache Lion <saachekapoor962011@gmail.com>
2025-08-13 20:14:47 +0530

    Numpy Start

--- ex.py
+++ ex.py
@@ -1,3 +1,15 @@
+#1
+fav_no = int(input("Enter your favorite number: "))
+print("Your favorite number is: ", fav_no)
+new_fav_no = fav_no * 2
+print("Your new favorite number is: ", new_fav_no)
+
+#2
+hobbies = input("Enter your hobbies separated by commas: ")
+hobbies_list = hobbies.split(',')
+for i, hobby in enumerate(hobbies_list, start=1):
+    print(f"{i} I love {hobby}")
+
 #3
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
